d9c368c5aff3e715c535ae3ff098b0a19bc2ede1
Cristian Sanclemente <cristian.sanclemente@interactiv4.com>
2024-02-26 16:08:40 +0100

    move install to global object

--- src/Migration/Setup.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const installAppSchema: (shop: string, token: string) => Promise<void>;
-export declare const uninstallAppSchema: (shop: string, token: string) => Promise<void>;
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,51 @@ import {MetaobjectSetupManager} from "./MetaobjectSetupManager";
 import {Definition} from "./Definition/Definition";
 import {MetaobjectRepository} from "./MetaobjectRepository";
 import {Metaobject} from "./Metaobject";
+import {readFileSync} from "fs";
 
 export const metaobject = {
+  schema: {
+    install: async (shop: string, token: string): Promise<void> => {
+      console.log('Installing app schema for shop: ' + shop);
+      const manager = new MetaobjectSetupManager(shop, token);
+      try {
+        const schema: string = readFileSync('metaobject/schema.json', 'utf8');
+        const definitions: Definition[] = JSON.parse(schema);
+        for (const definition of definitions) {
+          await Promise.all(definition.fields
+            .filter(field => 'metaobject_reference' === field.type)
+            .map(async field => {
+              let validation = field.validations.find(validation => validation.name === 'metaobject_definition_id');
+              if (validation && validation.value !== undefined) {
+                validation.value = await manager.getDefinitionId(validation.value as string) ?? '';
+              }
+            }));
+          await manager.createDefinition(definition);
+        }
+      } catch (e) {
+        console.error('No schema is not defined');
+        return;
+      }
+    },
+    uninstall: async (shop: string, token: string): Promise<void> => {
+      console.log('Uninstalling app schema for shop: ' + shop);
+      const manager = new MetaobjectSetupManager(shop, token);
+      try {
+        const schema: string = readFileSync('metaobject/schema.json', 'utf8');
+        const definitions: Definition[] = JSON.parse(schema);
+        for (const definition of definitions) {
+          const id = await manager.getDefinitionId(definition.type);
+          if (null === id) {
+            continue;
+          }
+          await manager.removeDefinition(id);
+        }
+      } catch (e) {
+        console.error('No schema is not defined');
+        return;
+      }
+    }
+  },
   definition: {
     save: async function (shop: string, token: string, definition: Definition): Promise<void> {
       const repository = new MetaobjectSetupManager(shop, token);
